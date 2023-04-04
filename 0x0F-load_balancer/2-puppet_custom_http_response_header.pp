7cfc19f5e2dba44364782e1e5a297482e40423b9
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-04 04:15:59 -0700

    a puppet creating custom http header response

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,39 @@
+exec { 'update':
+  command => 'apt-get update',
+  path    => ['/bin', '/usr/bin/'],
+  before  => Exec['install_nginx'],
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+}
+
+server_tokens off;
+
+add_header X-Served-By $hostname;
+
+",
+  notify => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => Package['nginx'],
+}
